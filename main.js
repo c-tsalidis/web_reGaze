40f472f7a67c960ade586849c18ebc07256217ba
c-tsalidis <ctsalidis1999@gmail.com>
2021-01-31 20:10:45 +0100

    Added some parallax scrolling effect

--- main.js
+++ main.js
@@ -1,34 +1,21 @@
+
+/*
 // init
 var controller = new ScrollMagic.Controller();
 
-/*
-// define movement of panels
-var wipeAnimation = new TimelineMax()
-    .fromTo("section.panel.turqoise", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
-    .fromTo("section.panel.green",    1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
-    .fromTo("section.panel.bordeaux", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}); // in from top
 
-// create scene to pin and link animation
-new ScrollMagic.Scene({
-        triggerElement: "#pinContainer",
-        triggerHook: "onLeave",
-        duration: "300%"
-    })
-    .setPin("#pinContainer")
-    .setTween(wipeAnimation)
-    .addIndicators() // add indicators (requires plugin)
+// build tween
+var tween = TweenMax.fromTo("#animate1", 1,
+    { left: -500 },
+    { left: 500, repeat: -1, yoyo: true, ease: Circ.easeInOut }
+);
+
+// build scene
+var scene = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 200, offset: -50 })
+    .setTween(tween)
+    .addIndicators({ name: "loop" }) // add indicators (requires plugin)
     .addTo(controller);
+
 */
 
-// build scenes
-var revealElements = document.getElementsByClassName("digit");
-for (var i=0; i<revealElements.length; i++) { // create a scene for each element
-    new ScrollMagic.Scene({
-                        triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
-                        offset: 50,												 // start a little later
-                        triggerHook: 0.9,
-                    })
-                    .setClassToggle(revealElements[i], "visible") // add class toggle
-                    .addIndicators({name: "digit " + (i+1) }) // add indicators (requires plugin)
-                    .addTo(controller);
-}
+var rellax = new Rellax('.rellax');
